73c1454defc7cc328ed7a512fb9bece1c0a85283
Koushik Bhoumik <kaushik.bhaumik@sustainedgeconsulting.com>
2026-05-13 17:09:46 +0530

    refactor: moved config file to shared folder

--- src/loan_sales_agent_shared/config.py
+++ src/loan_sales_agent_shared/config.py
@@ -18,7 +18,7 @@ os.makedirs(os.getenv("SANCTION_LETTER_PATH"), exist_ok=True)
 SANCTION_LETTER_PATH = os.getenv("SANCTION_LETTER_PATH")
 
 # Database Config
-DATABASE_URL = f"postgresql://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@{os.getenv("POSTGRES_HOST")}:{os.getenv("POSTGRES_PORT")}/{os.getenv("POSTGRES_DATABASE")}"
+DATABASE_URL = f"postgresql://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@{os.getenv("POSTGRES_HOST")}:{os.getenv("POSTGRES_PORT")}/{os.getenv("POSTGRES_DB")}"
 TEST_DATABASE_URL = f"postgresql://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@{os.getenv("POSTGRES_HOST")}:{os.getenv("POSTGRES_PORT")}/nbfc_test_database"
 os.makedirs(os.getenv("DB_PATH"), exist_ok=True)
 
