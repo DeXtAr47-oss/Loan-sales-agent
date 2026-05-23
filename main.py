34913f4c813485899b6c4211a155834570d05d90
Pritam Das <79273068+DeXtAr47-oss@users.noreply.github.com>
2026-05-23 22:40:44 +0530

    refactor: main.py is now only be used for routes

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from src.loan_sales_agent_shared.connection import base, engine
-from src.loan_sales_agent_DL.models.credit_score_model import CreditScore, RelCreditScoreCustomer
-from src.loan_sales_agent_DL.models.customer_model import Customer
-from src.loan_sales_agent_DL.models.loan_application_model import LoanApplication, RelLoanApplicationCustomer
-from src.loan_sales_agent_DL.models.loan_offer_model import LoanOffer, RelLoanOfferCustomer
-from src.loan_sales_agent_DL.models.salary_slip_model import SalarySlip, RelSalarySlipCustomer, RelSalarySlipLoanApplication
+from fastapi import FastAPI
+from src.loan_sales_agent_controllers.Customer_controller import router as customer_router
+from src.loan_sales_agent_DL import *
+app = FastAPI()
 
-
-base.metadata.create_all(bind=engine)
-print('database created')
+app.get("/")
+def home():
+    return {"message": "Loan sales agent home page"}
+app.include_router(customer_router)
