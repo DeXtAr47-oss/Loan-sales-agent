0c17bafb8597250b17b9876ec550378758cddba5
Pritam Das <79273068+DeXtAr47-oss@users.noreply.github.com>
2026-05-31 01:54:42 +0530

    refactor: change many core logics

--- src/loan_sales_agent_DL/repository/credit_score_repository.py
+++ src/loan_sales_agent_DL/repository/credit_score_repository.py
@@ -8,46 +8,26 @@ def get_credit_scores(db: Session):
     return db.query(CreditScore.credit_score).all()
 
 
-def get_credit_score(db: Session, customer_id: UUID):
-    return (
-        db.query(CreditScore)
-        .join(
-            RelCreditScoreCustomer,
-            CreditScore.credit_score_id ==
-            RelCreditScoreCustomer.credit_score_id
-        )
-        .join(
-            Customer,
-            Customer.customer_id ==
-            RelCreditScoreCustomer.customer_id
-        )
-        .filter(Customer.customer_id == customer_id)
-        .all()
-    )
+def get_credit_score(db: Session, credit_score_id: int):
+    result = (db.query(CreditScore)
+              .filter(CreditScore.credit_score_id == credit_score_id)
+              .first())
+    return result
 
 
-def set_credit_score(db: Session, cust_id: UUID, credit_score: CreditScoreCreate):
+
+def set_credit_score(db: Session, credit_score: CreditScoreCreate):
     new_credit_score = CreditScore(
         credit_score=credit_score.credit_score
     )
     db.add(new_credit_score)
     db.flush()
 
-    rel_credit_score_customer = RelCreditScoreCustomer(
-        customer_id=cust_id,
-        credit_score_id=new_credit_score.credit_score_id
-    )
-
-    db.add(rel_credit_score_customer)
-    db.commit()
-    db.refresh(new_credit_score)
-    db.refresh(rel_credit_score_customer)
-
     return new_credit_score
 
 
-def update_credit_score(db: Session, cust_id: int, credit_score: CreditScoreBase):
-    db_credit_score = get_credit_score(db, cust_id)
+def update_credit_score(db: Session, credit_id: int, credit_score: CreditScoreBase):
+    db_credit_score = get_credit_score(db, credit_id)
     if not db_credit_score:
         return None
 
