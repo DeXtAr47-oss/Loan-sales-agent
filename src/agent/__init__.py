2ee806e83371412d64975057a006ae89d3368a26
Koushik Bhoumik <kaushik.bhaumik@sustainedgeconsulting.com>
2026-06-11 16:10:20 +0530

    feat: add packages

--- src/agent/__init__.py
+++ src/agent/__init__.py
@@ -1,4 +1,4 @@
-from state import LoanState
-from graph import create_loan_workflow
+from agent.state_and_graph.state import LoanState
+from agent.state_and_graph.graph import create_loan_workflow
 
 __all__ = ["LoanState", "create_loan_workflow"]
