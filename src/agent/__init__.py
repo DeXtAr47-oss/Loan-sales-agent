e60472ec4c44f86733e1a8b15a0d594657d227b6
Koushik Bhoumik <kaushik.bhaumik@sustainedgeconsulting.com>
2026-06-11 18:03:24 +0530

    refactor: package directory add

--- src/agent/__init__.py
+++ src/agent/__init__.py
@@ -1,4 +1,4 @@
-from agent.state_and_graph.state import LoanState
-from agent.state_and_graph.graph import create_loan_workflow
+from src.agent.states.state import LoanState
+from src.agent.graphs.graph import create_loan_workflow
 
 __all__ = ["LoanState", "create_loan_workflow"]
