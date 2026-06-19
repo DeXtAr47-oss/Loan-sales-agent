3a28e0a6a2475ac9b644e468dad6584e9f3c430d
Pritam Das <79273068+DeXtAr47-oss@users.noreply.github.com>
2026-06-20 01:47:00 +0530

    feat: add common auth shared code create_access_token and verify_token

---
+++ src/loan_sales_agent_shared/auth.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
+
+from src.loan_sales_agent_shared.config import AUTH_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+    to_encode.update({"exp": expire})
+    return jwt.encode(
+        to_encode,
+        AUTH_KEY,
+        algorithm=ALGORITHM
+    )
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(
+            token,
+            AUTH_KEY,
+            algorithms=[ALGORITHM]
+        )
+        return payload
+    except JWTError as e:
+        return None 
