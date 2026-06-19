7a8ee5750d0345a805adf0116e189348da0701e3
Pritam Das <79273068+DeXtAr47-oss@users.noreply.github.com>
2026-06-20 01:51:38 +0530

    feat: add sigin router

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi.responses import FileResponse
 from src.loan_sales_agent_API.Customer_controller import router, api_router
 from src.loan_sales_agent_API.Chat_controller import chat_router
 from src.loan_sales_agent_API.Credit_score_controller import credit_score_api_router
+from src.loan_sales_agent_API.Signin_controller import signin_router, api_signin_router
 from src.loan_sales_agent_DL import *
 app = FastAPI()
 
@@ -21,7 +22,8 @@ app.include_router(router)
 app.include_router(api_router)
 app.include_router(credit_score_api_router)
 app.include_router(chat_router)
-
+app.include_router(api_signin_router)
+app.include_router(signin_router)
 app.mount(
     "/static",
     StaticFiles(directory="frontend/build/static"),
