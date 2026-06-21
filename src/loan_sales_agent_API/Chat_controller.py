75b185fb486e264863293b3fd422e0d9f7d33679
Pritam Das <79273068+DeXtAr47-oss@users.noreply.github.com>
2026-06-21 14:23:52 +0530

    refactor: changed code logic in chat endpoint

--- src/loan_sales_agent_API/Chat_controller.py
+++ src/loan_sales_agent_API/Chat_controller.py
@@ -1,32 +1,35 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, Request
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.loan_sales_agent_shared.connection import get_db
-from src.agent.graphs.master_agent_graph import create_master_agent_workflow
-from src.agent.states.state import LoanState
+from src.loan_sales_agent_BL.services.chat_service import chat_service
 from src.loan_sales_agent_BL.schemas.chat_schema import ChatResponse, ChatRequest
+from src.loan_sales_agent_BL.services.authentication_service import get_current_user, get_current_user_api
 
 chat_router = APIRouter(
     prefix="/chat",
-    tags=["chat"]
+    tags=["chat"],
+    include_in_schema=False
 )
 
-@chat_router.post("/", response_model=ChatResponse)
-async def chat(request: ChatRequest, db: Session = Depends(get_db)):
-    graph = create_master_agent_workflow(db)
+api_chat_router = APIRouter(
+    prefix="/api/chat",
+    tags=["Chat API"]
+)
 
-    initial_state: LoanState = {
-        "messages": [{"role": "user", "content": request.message}]
-    }
+@chat_router.post("", response_model=ChatResponse)
+async def chat(request: ChatRequest,
+               http_request: Request,
+               db: AsyncSession = Depends(get_db),
+               current_user = Depends(get_current_user_api)
+               ):
+    graph = http_request.app.state.graph
+    return await chat_service(graph, request, current_user)
 
-    result = await graph.ainvoke(
-        initial_state,
-        config={"configurable": {"thread_id": request.thread_id}}
-    )
-
-    last_message = result["messages"][-1]
-
-    return ChatResponse(
-        reply=last_message.content if hasattr(last_message, "content") else str(last_message),
-        state=dict(result)
-    )
+@api_chat_router.post("/", response_model=ChatResponse)
+async def chat(request: ChatRequest,
+               http_request: Request,
+               db: AsyncSession = Depends(get_db),
+               current_user = Depends(get_current_user_api)):
+    graph = http_request.app.state.graph
+    return await chat_service(graph, request, current_user)
