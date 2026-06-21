6b7e82089319287270d7dac312675529fb4d71e1
Pritam Das <79273068+DeXtAr47-oss@users.noreply.github.com>
2026-06-21 14:29:23 +0530

    feat: add graph and add chat api endpoint in the main file

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from contextlib import asynccontextmanager
 
 from src.loan_sales_agent_API.Customer_controller import router, api_router
-from src.loan_sales_agent_API.Chat_controller import chat_router
+from src.loan_sales_agent_API.Chat_controller import chat_router, api_chat_router
 from src.loan_sales_agent_API.Credit_score_controller import credit_score_api_router
 from src.loan_sales_agent_API.Signin_controller import signin_router, api_signin_router
-from src.loan_sales_agent_DL import *
-app = FastAPI()
+from src.agent.graphs.master_agent_graph import create_master_agent_workflow
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.graph = create_master_agent_workflow()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,8 +29,10 @@ app.include_router(router)
 app.include_router(api_router)
 app.include_router(credit_score_api_router)
 app.include_router(chat_router)
+app.include_router(api_chat_router)
 app.include_router(api_signin_router)
 app.include_router(signin_router)
+
 app.mount(
     "/static",
     StaticFiles(directory="frontend/build/static"),
@@ -31,6 +40,6 @@ app.mount(
 )
 
 # SERVE REACT APP
-@app.get("/{full_path:path}")
+@app.get("/{full_path:path}", include_in_schema=False)
 async def serve_react_app(full_path: str):
     return FileResponse("frontend/build/index.html")
