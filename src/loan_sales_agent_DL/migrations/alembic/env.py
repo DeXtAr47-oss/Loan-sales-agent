51850f5af6c9970e8b9056867e4dca83b6d97d9f
Pritam Das <79273068+DeXtAr47-oss@users.noreply.github.com>
2026-06-22 16:00:49 +0530

    refactor: change the path of database for migration

--- src/loan_sales_agent_DL/migrations/alembic/env.py
+++ src/loan_sales_agent_DL/migrations/alembic/env.py
@@ -12,7 +12,15 @@ from src.loan_sales_agent_DL.models import *
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", DATABASE_URL)
+sync_database_url = DATABASE_URL.replace(
+    "postgresql+asyncpg",
+    "postgresql+psycopg2"
+)
+
+config.set_main_option(
+    "sqlalchemy.url",
+    sync_database_url
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
