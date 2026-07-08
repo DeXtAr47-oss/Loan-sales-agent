8bb3d023119c1c7d436b660c7e26b80e5b59e285
Koushik Bhoumik <kaushik.bhaumik@sustainedgeconsulting.com>
2026-07-08 14:33:24 +0530

    feat: add import of permission module

--- src/loan_sales_agent_DL/models/__init__.py
+++ src/loan_sales_agent_DL/models/__init__.py
@@ -3,3 +3,4 @@ from .credit_score_model import CreditScore, RelCreditScoreCustomer
 from .loan_application_model import LoanApplication, RelLoanApplicationCustomer
 from .loan_offer_model import LoanOffer, RelLoanOfferCustomer
 from .salary_slip_model import SalarySlip, RelSalarySlipCustomer, RelSalarySlipLoanApplication
+from .permission_model import Permission, RelPermissionCustomer
